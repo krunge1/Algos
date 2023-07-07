195100b5002c402049d210eee71bf85a52b76b72
Kyle Runge <krunge1@hotmail.com>
2023-07-07 12:50:32 -0500

    Bonus remove at function

--- arrasy1.js
+++ arrasy1.js
@@ -6,10 +6,10 @@
 // pushFront([5,7,2,3], 8) => [8,5,7,2,3]
 // pushFront([99], 7) => [7,99]
 
-let arr = [5,7,2,3]
+let arr = [6,5,7,2,3]
 
 function pushFront(arr, value){
-    for (i=arr.length; i>0; i--){
+    for (let i=arr.length; i>0; i--){
         arr[i]=arr[i-1]
     }
     arr[0] = value
@@ -27,7 +27,7 @@ function pushFront(arr, value){
 // popFront([4,5,7,9]) => 4 returned, with [5,7,9] printed in the function
 function popFront(arr){
     let first = arr[0]
-    for (i=0; i<arr.length; i++){
+    for (let i=0; i<arr.length; i++){
         arr[i]=arr[i+1]
     }
     arr.length= arr.length-1
@@ -35,7 +35,7 @@ function popFront(arr){
     return first
 }
 
-console.log(popFront(arr))
+// console.log(popFront(arr))
 
 // Insert At
 // Given an array, index, and additional value, insert the value into array at given index. You can think of pushFront(arr,val) as equivalent to insertAt(arr,0,val). You may use .push(), you are able do this without it though!
@@ -45,11 +45,31 @@ console.log(popFront(arr))
 // insertAt([100,200,5], 2, 311) => [100,200,311,5]
 // insertAt([9,33,7], 1, 42) => [9,42,33,7]
 function insertAt(arr, location, value) {
-    for (i=arr.length; i>location-1; i--){
+    for (let i=arr.length; i>location-1; i--){
         arr[i]=arr[i-1]
     }
     arr[location-1] = value
     return arr;
 }
 
-// console.log(insertAt(arr, 15, 4))
+// console.log(insertAt(arr, 4, 254))
+
+// BONUS: Remove At
+// Given an array and an index into array, remove and return the array value at that index. Prove the value is removed from the array by printing it. Think of popFront(arr) as equivalent to removeAt(arr,0).
+
+// Examples:
+
+// removeAt([1000,3,204,77], 1) => 3 returned, with [1000,204,77] printed in the function
+// removeAt([8,20,55,44,98], 3) => 44 returned, with [8,20,55,98] printed in the function
+
+function removeAt(arr, location){
+    let removedValue = arr[location-1]
+    for (let i=location-1; i<arr.length; i++){
+        arr[i]=arr[i+1]
+    }
+    console.log(removedValue)
+    arr.length= arr.length-1
+    return arr;
+}
+
+console.log(removeAt(arr, 2))
